843263f45725d1b2b51d22f5481f29fa7544ddb9
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-29 17:27:10 +0300

    Updated all files or directory in react project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './assets/style/Style.module.scss'
+
+function App() {
+  return (
+    <div className={classes.wrapper}>
+      text
+    </div>
+  );
+}
+
+export default App;
