f4ff9ee9a8dc00adeb1d7bf0a0ebb69bfd8aec55
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-10 23:33:25 +0300

    Install images

--- src/@types/assets.d.ts
+++ src/@types/assets.d.ts
@@ -1,8 +1,14 @@
 //Images====================================================================================================
 
 declare module '*.svg' {
-    const content: any;
-    export default content;
+    import * as React from 'react';
+
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<
+        SVGSVGElement
+        > & { title?: string }>;
+
+    const src: string;
+    export default src;
 }
 
 declare module '*.png' {
